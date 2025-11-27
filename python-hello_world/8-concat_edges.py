8100c33821dfcb78489a11f993472eae981c304f
roya86 <royaabduallah@gmail.com>
2025-11-27 10:37:42 +0000

    Use slicing in a single print statement

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 word = "object-oriented programming with Python"
-word_first_6 = word[:6]
-word_next_1 = word[6:7]
-word_middle = word[7:-7]
-word_last_7 = word[-7:]
-print("{}{}{} {}".format(word_first_6, word_next_1, word_middle, word_last_7))
+# slicing and printing in one line
+# no new variables allowed
+print(word[:6] + word[6:7] + word[7:-7] + " " + word[-7:])
