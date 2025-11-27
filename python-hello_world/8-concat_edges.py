a36ad2c475ab4007d7ce941c081ac3554121e3f5
roya86 <royaabduallah@gmail.com>
2025-11-27 10:20:54 +0000

    Add script that rebuilds text using string slicing

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+word = "object-oriented programming with Python"
+word_first_6 = word[:6]
+word_next_1 = word[6:7]
+word_middle = word[7:-7]
+word_last_7 = word[-7:]
+print("{}{}{} {}".format(word_first_6, word_next_1, word_middle, word_last_7))
