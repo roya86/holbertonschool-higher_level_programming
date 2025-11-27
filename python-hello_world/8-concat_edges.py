65ae41e9720eaf6de3df7e5013c2422a70736168
roya86 <royaabduallah@gmail.com>
2025-11-27 10:43:11 +0000

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 word = "object-oriented programming with Python"
-# slicing and printing in one line
-# no new variables allowed
-print(word[:6] + word[6:7] + word[7:-7] + " " + word[-7:])
+print(word[:6] + word[6:7] + word[7:-7] + word[20:21] + word[-7:])
