1b5617632aa6e1a6808aaec1ddd590d51ca21c1a
roya86 <royaabduallah@gmail.com>
2025-12-07 07:30:56 +0000

    Add Task 4

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+    Function divides all elements of a matrix
+
+    Example:
+       first_name = "Khiba"
+       last_name = "Koenane"
+
+       say_my_name(first_name, last_name)
+
+       Khiba Koenane
+"""
+
+
+def text_indentation(text):
+    """
+        Args:
+            first_name: first name
+            last_name: second name
+
+        Returns:
+            A sentence with the first and last name
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
