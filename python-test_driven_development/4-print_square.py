d39a3bf3d575d9465af7a7c788436e9c3a563c05
roya86 <royaabduallah@gmail.com>
2025-12-07 07:17:29 +0000

    Add Task 3

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+    Function divides all elements of a matrix
+
+    Example:
+       size = 2
+
+       print_square(size)
+
+       ##
+       ##
+"""
+
+
+def print_square(size):
+    """
+        Args:
+            size: size of square
+
+        Returns:
+            a square the size of square arg
+    """
+    height = 0
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    while height < size:
+        width = 0
+        while width < size:
+            print("#", end="")
+            width += 1
+        print()
+        height += 1
