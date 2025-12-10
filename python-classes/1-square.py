750cdcf159717bf4cf9de18a058ada9da9186cf5
roya86 <royaabduallah@gmail.com>
2025-12-10 07:15:30 +0000

    Add  a class Square that defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+1-square.py : is a class Square that defines a square
+"""
+
+
+class Square:
+    """class Square that defines a square
+
+    Attributes:
+    attr1 (size): Size of the Square
+
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
