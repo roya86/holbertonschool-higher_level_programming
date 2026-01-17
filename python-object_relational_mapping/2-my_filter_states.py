c437cae9735e1ca8e73b437802b11c1c08a3f69e
roya86 <royaabduallah@gmail.com>
2026-01-17 13:58:42 +0000

    task2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -13,6 +13,9 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
+    # إزالة علامات الاقتباس
+    state_name = state_name.strip("'")
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -23,9 +26,11 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        state_name
-    )
+    query = (
+        "SELECT * FROM states "
+        "WHERE name = BINARY '{}' "
+        "ORDER BY id ASC"
+    ).format(state_name)
 
     cur.execute(query)
     states = cur.fetchall()
