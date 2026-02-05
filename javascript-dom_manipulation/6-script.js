c3ff0552a5c0dfffec786e9856a4eafb93794d4e
roya86 <royaabduallah@gmail.com>
2026-02-05 12:23:35 +0300

    Task 6

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,10 @@
+const characterDiv = document.querySelector('#character');
+
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    characterDiv.textContent = data.name;
+  })
+  .catch(error => {
+    characterDiv.textContent = 'Error loading character';
+  });
