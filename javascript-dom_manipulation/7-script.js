11fe2ba64409e2fa418e7ccf789798df969b0ab1
roya86 <royaabduallah@gmail.com>
2026-02-05 12:25:44 +0300

    Task 7

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,14 @@
+const ul = document.querySelector('#list_movies');
+
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    data.results.forEach(film => {
+      const li = document.createElement('li');
+      li.textContent = film.title;
+      ul.appendChild(li);
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
