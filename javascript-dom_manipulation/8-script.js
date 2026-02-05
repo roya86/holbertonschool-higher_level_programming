5120acea332734347cfb002a02efae517c041003
roya86 <royaabduallah@gmail.com>
2026-02-05 12:27:57 +0300

    Task 8

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const helloDiv = document.querySelector('#hello');
+
+  fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
+    .then(response => response.json())
+    .then(data => {
+      helloDiv.textContent = data.hello;
+    })
+    .catch(() => {
+      helloDiv.textContent = 'Error';
+    });
+});
